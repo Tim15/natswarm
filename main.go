7fea7465a14b6f39e4813b2083685cd096a62749
Timothy Higinbottom <timhigins@gmail.com>
2017-10-12 09:32:01 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
   "github.com/docker/swarmkit/agent"
+  "github.com/docker/swarmkit/api"
   "fmt"
   "os"
+  "context"
 )
+
+// LocalExecutor implements agent/exec#Executor
+type LocalExecutor struct
+
+func (*LocalExecutor) Describe(ctx context.Context) (*api.NodeDescription, error) {
+  return api.NodeDescription{}
+}
+// Configure uses the node object state to propagate node
+// state to the underlying executor.
+Configure(ctx context.Context, node *api.Node) error
+
+// Controller provides a controller for the given task.
+Controller(t *api.Task) (Controller, error)
+
+// SetNetworkBootstrapKeys passes the symmetric keys from the
+// manager to the executor.
+SetNetworkBootstrapKeys([]*api.EncryptionKey) error
 
 func main() {
   fmt.Println("Starting natswarm")
