ae95004880be56309093d93f3bb823edfd00fa3f
Timothy Higinbottom <timhigins@gmail.com>
2017-10-12 09:27:55 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+  "github.com/docker/swarmkit/agent"
+  "fmt"
+  "os"
+)
+
 func main() {
-]
+  fmt.Println("Starting natswarm")
+  fmt.Println("Bootstrapping local agent")
+  hostname := os.Hostname()
+  cfg := agent.Config{
+    Hostname: hostname,
+  }
+  a, err := agent.Agent.New(cfg)
+  if err != nil {
+    fmt.Println(err)
+  }
+}
