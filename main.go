1a883388dbc9d45e70d323b121cbc39b20d97e1e
Timothy Higinbottom <timhigins@gmail.com>
2017-10-13 17:51:18 -0400

    more work

--- main.go
+++ main.go
@@ -1,39 +1,63 @@
 package main
 
 import (
-  "github.com/docker/swarmkit/agent"
-  "github.com/docker/swarmkit/api"
-  "fmt"
-  "os"
-  "context"
+	"context"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/docker/swarmkit/agent"
+	"github.com/docker/swarmkit/agent/exec"
+	"github.com/docker/swarmkit/api"
+	"google.golang.org/grpc/credentials"
+	"os"
 )
 
 // LocalExecutor implements agent/exec#Executor
-type LocalExecutor struct
+type LocalExecutor struct{}
 
 func (*LocalExecutor) Describe(ctx context.Context) (*api.NodeDescription, error) {
-  return api.NodeDescription{}
+	return &api.NodeDescription{}, nil
 }
+
 // Configure uses the node object state to propagate node
 // state to the underlying executor.
-Configure(ctx context.Context, node *api.Node) error
+func (*LocalExecutor) Configure(ctx context.Context, node *api.Node) error {
+	return nil
+}
 
 // Controller provides a controller for the given task.
-Controller(t *api.Task) (Controller, error)
+func (*LocalExecutor) Controller(t *api.Task) (exec.Controller, error) {
+	return nil, nil
+}
 
 // SetNetworkBootstrapKeys passes the symmetric keys from the
 // manager to the executor.
-SetNetworkBootstrapKeys([]*api.EncryptionKey) error
+func (*LocalExecutor) SetNetworkBootstrapKeys([]*api.EncryptionKey) error {
+	return nil
+}
 
 func main() {
-  fmt.Println("Starting natswarm")
-  fmt.Println("Bootstrapping local agent")
-  hostname := os.Hostname()
-  cfg := agent.Config{
-    Hostname: hostname,
-  }
-  a, err := agent.Agent.New(cfg)
-  if err != nil {
-    fmt.Println(err)
-  }
+	fmt.Println("Starting natswarm")
+	fmt.Println("Bootstrapping local agent")
+	hostname, _ := os.Hostname()
+	b := &x509.Certificate{}
+
+	cert, err := x509.CreateCertificate(rand.Reader, nil, nil, nil, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tcert, err := tls.X509KeyPair(cert, keyPEMBlock)
+	if err != nil {
+		fmt.Println(err)
+	}
+	cfg := agent.Config{
+		Hostname:    hostname,
+		Credentials: credentials.NewServerTLSFromCert(&tcert),
+	}
+	a, err := agent.New(&cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(a)
 }
